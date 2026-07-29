67a3f9cea02f3041d566167ea5c85d114b34f134
mohammad <khdm.mohammad@gmail.com>
2026-07-29 17:53:15 +0330

    s16

---
+++ s16/app.js
@@ -0,0 +1,137 @@
+// var number = 18;
+// if (number % 2 === 0) {
+//     console.log("even");
+// } else {
+//     console.log("odd");
+// }
+
+// var weekDay = 2;
+// switch (weekDay) {
+//     case 3:
+//     case 4:
+//         console.log("soon weekend");
+//         break;
+//     case 5:
+//     case 6:
+//         console.log("weekend");
+//         break;
+//     default:
+//         console.log("looking for weekend");
+// }
+
+// var cart = [
+//     { name: "chips", price: 5 },
+//     { name: "cake", price: 1 },
+//     { name: "ice cream", price: 3 },
+//     { name: "ice cream2", price: 3 },
+//     { name: "ice cream3", price: 4 },
+//     { name: "ice cream4", price: 4 },
+//     { name: "ice cream5", price: 3.5 },
+//     { name: "ice cream6", price: 1 },
+//     { name: "ice cream7", price: 2 },
+// ];
+
+// var sumPrice = cart[0].price + cart[1].price + cart[2].price + cart[3].price;
+// if (sumPrice >= 10) {
+//     console.log("1% discount");
+// } else {
+//     console.log("main price");
+// }
+
+// for (var i = 0; i < 5; i++) {
+//     console.log(i);
+// }
+
+// var students = ["sina", "kimia", "sanaz", "aria"];
+// console.log(students[0])
+// for (var i = 0; i < 4; i++) console.log(students[i]);
+
+// var numbers = [2, 5, 4, 3];
+// var sum = numbers[0] + numbers[1] + numbers[2] + numbers[3];
+
+// var sum = 0;
+// for (var i = 0; i < 4; i++) {
+//     sum = sum + numbers[i];
+// }
+// console.log(sum);
+
+// var cart = [
+//     { name: "chips", price: 5 },
+//     { name: "cake", price: 1 },
+//     { name: "ice cream", price: 3 },
+//     { name: "ice cream2", price: 3 },
+//     { name: "ice cream3", price: 4 },
+//     { name: "ice cream4", price: 4 },
+//     { name: "ice cream5", price: 3.5 },
+//     { name: "ice cream6", price: 1 },
+//     { name: "ice cream7", price: 2 },
+// ];
+
+// var sum = 0;
+// for (var i = 0; i < cart.length; i++) {
+//     sum += cart[i].price;
+// }
+// if (sum > 10) {
+//     console.log("discount");
+// } else {
+//     console.log("none");
+// }
+
+// var i = 0;
+// while (i < 1000) {
+//     console.log(i);
+//     i++;
+// }
+
+// var i = 0;
+// do {
+//     console.log(i);
+//     i++;
+// } while (i < 0);
+
+// while (i < 0) {
+//     console.log(i);
+//     i++;
+// }
+
+// آرایه از اعداد داریم. هر عدد چندبار تکرار شده است
+// آرایه ای از اعداد داریم. بزرگترین عدد زوج و کوچکترین عدد فرد
+
+// var cartUser1 = [
+//     { name: "chips", price: 5 },
+//     { name: "cake", price: 1 },
+// ];
+
+// if (cartUser1[0].price + cartUser1[1].price > 5) {
+//     console.log("dicount");
+// } else {
+//     console.log("none");
+// }
+
+// var cartUser2 = [
+//     { name: "ice cream", price: 4 },
+//     { name: "biscoite", price: 4 },
+// ];
+
+// if (cartUser1[0].price + cartUser1[1].price > 5) {
+//     console.log("dicount");
+// } else {
+//     console.log("none");
+// }
+
+// var cartUser3 = [
+//     { name: "chips", price: 4 },
+//     { name: "biscoite", price: 2 },
+// ];
+
+// if (cartUser1[0].price + cartUser1[1].price > 5) {
+//     console.log("dicount");
+// } else {
+//     console.log("none");
+// }
+
+function print() {
+    console.log("hello");
+}
+
+//print(); // call, invoke
